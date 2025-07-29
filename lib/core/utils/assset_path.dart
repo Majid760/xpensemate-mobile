ef9df330202b66d8de5884c9aa1c3ba9ea5948ad
Majid760 <themuhmand@gmail.com>
2025-07-29 23:47:26 +0500

    chore(assetpath): asset path and class are added

---
+++ lib/core/utils/assset_path.dart
@@ -0,0 +1,18 @@
+// lib/core/constants/asset_paths.dart
+class AssetPaths {
+
+ 
+  // Images
+  static const String logo = 'assets/images/logo.png';
+  static const String userPlaceholder = 'assets/images/user_placeholder.png';
+  
+  // Icons
+  static const String homeIcon = 'assets/images/icons/home.svg';
+  static const String profileIcon = 'assets/images/icons/profile.svg';
+  
+  // Data
+  static const String countriesJson = 'assets/data/countries.json';
+  
+  // Fonts
+  static const String primaryFont = 'CustomFont';
+}
